c1e3029f19cf4cafabdfa9be3514d0940607ed0a
Mashashi Nakamura <mashashi.nakamura@gmail.com>
2015-09-27 23:39:40 +0100

    removed unnecesary write on argument variable file

--- 5-filtered_ls/main.js
+++ 5-filtered_ls/main.js
@@ -8,12 +8,12 @@ fs.readdir(path, function(err, files){
 	if(err){
 		console.log(err);
 	}
-	files = files.filter(function(file){
+
+	files.filter(function(file){
 		var extDot = file.indexOf('.');
 		if(extDot == -1){extDot=file.length-1;}
 		return file.substring(extDot+1) == ext;
-	});
-	files.forEach(function(file){
+	}).forEach(function(file){
 		console.log(file);
 	});
 });
